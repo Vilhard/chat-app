52315566ed7cba64e8d2d103ff4db8f2883d3770
Vilhard <ville.hotakainen@gmail.com>
2020-06-16 23:09:48 +0300

    moved chat.js

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
-import { View, StyleSheet, Alert } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet } from "react-native";
 import { GiftedChat, Bubble, Send } from "react-native-gifted-chat";
 import { IconButton } from "react-native-paper";
 import firebase from "../config/Firebase";
